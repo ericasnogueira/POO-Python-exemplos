5c2d9bfafa3e4cff00ae51745d71f3dd6fbb469f
ericasnogueira <ericasnogueira10@gmail.com>
2022-02-02 11:03:57 -0300

    Create O_que_são_módulos_e_qual_a_importância.py

---
+++ O_que_são_módulos_e_qual_a_importância.py
@@ -0,0 +1,19 @@
+"""
+O que são módulos e qual a importância deles ? 
+
+-Importância :
+    °Já tem muita coisa pronta, então não precisa criar do zero.
+    °Se souber usar módulos e como usar um módulo novo, vai conseguir fazer praticamente tudo no python.
+
+- Estrutura básica :
+
+    import módulo
+ou 
+    import módulo as nome
+
+"""
+#exemplo : como pode fazer o nosso código abrir um site específico na internet
+
+import webbrowser
+
+webbrowser.open('https://github.com/ericasnogueira')
